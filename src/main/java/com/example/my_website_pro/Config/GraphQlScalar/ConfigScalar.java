9fdadbf916207a3503104f113414e2d924ed9f58
truonghnn <truonghnnsr@gmail.com>
2024-02-13 21:57:24 +0700

    update graphql scalar

---
+++ src/main/java/com/example/my_website_pro/Config/GraphQlScalar/ConfigScalar.java
@@ -0,0 +1,20 @@
+package com.example.my_website_pro.Config.GraphQlScalar;
+
+import graphql.scalars.ExtendedScalars;
+import graphql.schema.GraphQLScalarType;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.graphql.execution.RuntimeWiringConfigurer;
+
+@Configuration
+public class ConfigScalar {
+
+    @Bean
+    public RuntimeWiringConfigurer runtimeWiringConfigurer() {
+        return wiringBuilder -> wiringBuilder
+                .scalar(ExtendedScalars.GraphQLBigDecimal)
+                .scalar(ExtendedScalars.Date)
+                .scalar(ExtendedScalars.DateTime);
+
+    }
+}
